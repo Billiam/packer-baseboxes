206841851334d19f950acddbdb7a126ceab6dc0d
Billiam <billiamthesecond@gmail.com>
2014-07-04 19:29:24 -0500

    Updating ruby, guest additions

--- squeeze/scripts/cleanup-virtualbox.sh
+++ squeeze/scripts/cleanup-virtualbox.sh
@@ -1,4 +1,4 @@
 # Cleanup Virtualbox
-VBOX_VERSION=$(cat .vbox_version)
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
 rm $VBOX_ISO
--- squeeze/scripts/ruby.sh
+++ squeeze/scripts/ruby.sh
@@ -1,23 +1,25 @@
 # Install ruby-build
 cd /home/vagrant
-git clone git://github.com/sstephenson/ruby-build.git
-cd ruby-build
-./install.sh
+wget -O ruby-install-0.4.3.tar.gz https://github.com/postmodern/ruby-install/archive/v0.4.3.tar.gz
+tar -xzvf ruby-install-0.4.3.tar.gz
+cd ruby-install-0.4.3/
+sudo make install
 
-# Install ruby 1.9.3
-ruby-build 1.9.3-p448 /opt/rubies/ruby-1.9.3-p448
+# Install ruby 2.1.2
+sudo ruby-install ruby-2.1.2
 
 cd /home/vagrant
-wget -O chruby-0.3.7.tar.gz https://github.com/postmodern/chruby/archive/v0.3.7.tar.gz
-tar -xzvf chruby-0.3.7.tar.gz
-cd chruby-0.3.7/
+wget -O chruby-0.3.8.tar.gz https://github.com/postmodern/chruby/archive/v0.3.8.tar.gz
+tar -xzvf chruby-0.3.8.tar.gz
+cd chruby-0.3.8/
 sudo make install
 
 # Set default ruby
-echo "source /usr/local/share/chruby/chruby.sh \nchruby ruby-1.9.3" | sudo tee /etc/profile.d/chruby.sh
+echo "source /usr/local/share/chruby/chruby.sh \nchruby ruby-2.1.2" | sudo tee /etc/profile.d/chruby.sh
 
-export RUBYCMD="source /usr/local/share/chruby/chruby.sh && chruby ruby-1.9.3"
+export RUBYCMD="source /usr/local/share/chruby/chruby.sh && chruby ruby-2.1.2"
 
-# Installing chef & Puppet
-#gem install chef --no-ri --no-rdoc
+# Installing Puppet
 sudo /bin/bash -c "$RUBYCMD && gem install puppet --no-ri --no-rdoc"
+
+exit 0
